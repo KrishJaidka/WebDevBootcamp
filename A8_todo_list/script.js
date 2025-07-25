3c5a6b2ce86b0fb6820a3b8034f7ae975c9e0840
KrishJaidka <krishjaidka9@gmail.com>
2025-07-25 11:19:31 +0530

    updated to-do list

--- A8_todo_list/script.js
+++ A8_todo_list/script.js
@@ -1,10 +1,38 @@
-        const input = document.querySelector('input'); 
-        const taskDiv = document.querySelector('#task-list');
-        const deleteButtonDiv = document.querySelector('#controls')
+const taskArray = [];
 
-        const add = () => {
-            taskDiv.innerHTML += `<li>${input.value}</li>`;
-            deleteButtonDiv.innerHTML += `button`
+const input = document.querySelector('input');
+const taskDiv = document.querySelector('#list');
+const deleteButtonDiv = document.querySelector('#controls')
 
-            input.value=""
-        }
+const createItemDiv = (taskText, taskIndex) => {
+    return `<div class="list-item">
+                <li>${taskText}</li>
+                <button class="delete" onClick="deleteTask(${taskIndex})">Delete</button>
+            </div>`
+}
+
+const add = () => {
+    if (input.value.trim() !== "") {
+        taskArray.push(input.value);
+        const taskIndex = taskArray.length - 1;
+        taskDiv.innerHTML += createItemDiv(input.value, taskIndex);
+        input.value = "";
+    }
+}
+
+const deleteTask = (index) => {
+    taskArray.splice(index, 1);
+    renderTasks();
+}
+
+const clearTask = () => {
+    taskArray.splice(0,taskArray.length);
+    renderTasks();
+}
+
+const renderTasks = () => {
+    taskDiv.innerHTML = "";
+    taskArray.forEach((task, index) => {
+        taskDiv.innerHTML += createItemDiv(task, index);
+    });
+}
